299cb2fccc1610fb6f33d1e5f90067982c5d7b72
Lisnyi <russelhobbbs@gmail.com>
2022-08-20 14:25:03 +0300

    add markup and fetch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,59 @@
 import { fetchImages } from './js/fetchImages'
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio'
 
 const form = document.querySelector("#search-form")
-const submitBtn = document.querySelector("button")
 const inputField = document.querySelector("input")
-const gallery = document.querySelector(".gallery")
+const galleryRef = document.querySelector(".gallery")
 
 form.addEventListener("submit", startSearch)
 
 function startSearch (e) {
     e.preventDefault()
+    let searchingValue = inputField.value.trim()
+    if (searchingValue !== "") {
+        fetchImages(searchingValue)
+        .then(data => {
+        if (data.data.totalHits === 0) {
+            failSearching()
+            return
+        }
+        successSearching(data.data.totalHits)
+        renderPhotoCard(data.data.hits)})
+    }
+}
+
+function successSearching (data) {
+    Notify.success(`Hooray! We found ${data} images.`)
+}
+
+function failSearching () {
+    Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+}
+
+function renderPhotoCard (data) {
+    const markup = data
+    .map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => ` 
+    <a href="${largeImageURL}">
+        <div class="photo-card">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes</b> ${likes}
+                </p>
+                <p class="info-item">
+                    <b>Views</b> ${views}
+                </p>
+                <p class="info-item">
+                    <b>Comments</b> ${comments}
+                </p>
+                <p class="info-item">
+                    <b>Downloads</b> ${downloads}
+                </p>
+            </div>
+        </div>
+    </a>
+        `)
+    .join("")
+
+    galleryRef.innerHTML = markup;
 }
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-const KEY = ''
-const BASE_URL = ''
-const FILTER = ''
+const KEY = '29392869-c497197ba4768b72814411005'
+const BASE_URL = 'https://pixabay.com/api/'
+const FILTER = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=40'
 
-function fetchImages () {
-    axios.get()
+function fetchImages (searchingValue) {
+    return axios.get(`${BASE_URL}?key=${KEY}&q=${searchingValue}&${FILTER}`)
 }
 
 export { fetchImages }
